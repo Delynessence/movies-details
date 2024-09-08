ca11e0667374641b45d21fa71322c5a52e277a21
Delynessence <riccopranata@gmail.com>
2024-09-08 18:55:24 +0700

    modif display

--- src/js/search.js
+++ src/js/search.js
@@ -55,7 +55,7 @@ document.querySelectorAll("nav ul a").forEach(item => {
                                     <div class="card-body">
                                         <h5 class="card-title">${data.Title}</h5>
                                         <h6 class="card-subtitle mb-2 text-muted">${data.Year}</h6>
-                                        <a href="#" class="btn btn-primary see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="${data.imdbID}" >See Details</a>
+                                        <a href="#" class="btn-detail see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="${data.imdbID}" >See Details<svg class="icon" viewBox="0 0 24 24" fill="currentColor"><path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z" clip-rule="evenodd"></path> </svg></a>
                                     </div>
                                 </div>
                             </div>
